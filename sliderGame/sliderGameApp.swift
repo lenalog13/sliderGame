8208fdf3f74c51f50bdfed79fb0e7054f18edbf9
lenalog13 <56520937+lenalog13@users.noreply.github.com>
2023-06-12 19:33:11 +0300

    Initial Commit

---
+++ sliderGame/sliderGameApp.swift
@@ -0,0 +1,17 @@
+//
+//  sliderGameApp.swift
+//  sliderGame
+//
+//  Created by Елена Логинова on 12.06.2023.
+//
+
+import SwiftUI
+
+@main
+struct sliderGameApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
